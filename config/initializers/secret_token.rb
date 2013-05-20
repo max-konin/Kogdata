6c66a8273f44c39d5f1522787f0d9b91236b237c
mkonin <maxim21214@gmail.com>
2013-05-20 17:25:56 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kogdata::Application.config.secret_token = '0be846a8a3cdf968d23e6a7f3e15f2dbe72411322b5cee7f79a1ab1536ce8f5cc61847086bae8c2401e08ede063b6c83a92c14fe9bf7e54302de5447429997df'
