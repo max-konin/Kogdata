f887ba1cb9c5f43ef0011c04ad944f04d8bc646c
IvanAdarich <ivan.adarich@gmail.com>
2013-05-30 18:34:05 +0700

    Govnocommit2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,1 +1,8 @@
-EventsController < ApplicationController
+class EventsController < ApplicationController
+
+  def new
+    @user = current_user
+    @event = @user.event.create(params[:event])
+
+  end
+end
