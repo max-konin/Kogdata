9d57d0c27a6535c18e876de928aca7bbdaf3fdbf
max-konin <qwas666>
2013-05-31 12:56:48 +0700

    add 2 ability: admin can manage all, contractor can upload photo

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,5 +29,16 @@ class Ability
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
 
+    user ||= User.new
+    if user.role? :admin
+      can :manage, :all
+    end
+
+    if user.role? :contractor
+      can :upload, :photo
+    end
+
+
+
   end
 end
