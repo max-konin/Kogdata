21173a289280ccdcd0d5acf87a6eb6f8d50c58f5
max-konin <qwas666>
2013-06-11 20:14:25 +0700

    add event page

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   attr_accessible :body,  :was_seen
-  belongs_to :user, :class_name => 'User', :foreign_key => 'sent_message_id'
-  belongs_to :user, :class_name => 'User', :foreign_key => 'received_messages_id'
+  belongs_to :recipient, :class_name => 'User', :foreign_key => 'user_recipient_id'
+  belongs_to :sender, :class_name => 'User', :foreign_key => 'user_sender_id'
 
 end
