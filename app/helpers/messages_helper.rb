69d8fb70046076f12b9c601b50403d662a37e297
max-konin <qwas666>
2013-06-28 15:20:18 +0700

    fix problem with send message btn on users#index page

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,8 +1,8 @@
 module MessagesHelper
   def count_unread_messages
     count = 0
-    converstions = current_user.conversations
-    converstions.each do |conv|
+    conversations = current_user.conversations
+    conversations.each do |conv|
       count += (conv.messages.where('user_id != ? AND was_seen = ?',current_user.id, false)).count
     end
     (count == 0)? '' : '(' + count.to_s + ')'
