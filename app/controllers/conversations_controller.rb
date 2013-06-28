a84f2e831b5cdf0330749ebdd9bf3eb0123afb35
max-konin <qwas666>
2013-06-28 14:36:53 +0700

    delete old method

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -28,31 +28,5 @@ class ConversationsController < ApplicationController
     redirect_to :back
   end
 
-  private
-  def find_or_create_conversation!
-
-    if params[:id].nil? then
-      members = params[:members]
-      members << current_user.id.to_s
-      members.sort!
-      @hash = String.new
-      members.each do |member|
-        @hash += (member.to_s + ' ')               #makes hash string from accepted ids of declared members and current user id
-      end
-
-      @conversation = current_user.conversations.find_by_hash_string(@hash)
-
-      if @conversation.nil? then
-        @conversation = current_user.conversations.build              #creates a new conversation for current user if no matches found
-        @conversation.hash_string = @hash
-        params[:members].each do |member|
-          @conversation.users << User.find(member)
-        end
-        @conversation.save!
-      end
-    else
-      @conversation = current_user.conversations.find_by_id(params[:id])
-    end
-  end
 
 end
